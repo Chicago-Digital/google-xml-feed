dc496f2d1a652da6f029f3000da6a5f130c2819c
developertenzin <tenzinphuljung7@gmail.com>
2017-06-08 15:41:20 -0500

    Add post route for testing purposes.

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res) {
 	res.send("Hi there! Seems like your app is working!");
 });
 
+router.post('/testing', function(req, res) {
+	res.send("This is the post office.");
+});
+
 module.exports = router;
